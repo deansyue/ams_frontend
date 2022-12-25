d48d861c64d6b696a57dd13d8a9de3a4ce2af31f
deanSyue <c1318131801@gmail.com>
2022-12-25 14:55:04 +0800

    feat: 新增首頁

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,18 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import NotFund from '../views/NotFund.vue'
 import SignIn from '../views/SignIn.vue'
+import HomePage from '../views/HomePage.vue'
 
 const routes = [
   {
     path: '/',
     name: 'sign-in',
     component: SignIn
+  },
+  {
+    path: '/HomePage',
+    name: '/home-page',
+    component: HomePage
   },
   {
     path: '/:pathMatch(.*)*',
---
+++ src/views/HomePage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container py-5">
+    <NavBar />
+    <div class="mt-5">
+      <h1 class="text-center">最新消息</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+import NavBar from "../components/NavBar.vue";
+
+export default {
+  name: "HomePage",
+  components: {
+    NavBar,
+  },
+};
+</script>
