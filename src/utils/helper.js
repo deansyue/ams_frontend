4ad22e6d1177f289afed6f30e17641a30b294e28
deanSyue <c1318131801@gmail.com>
2023-01-03 23:28:14 +0800

    feat: api url調整至線上部署url

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import Swal from 'sweetalert2'
 
-export const baseURL = 'http://localhost:3000/api'
+export const baseURL = 'https://stark-temple-81519.herokuapp.com/api'
 
 export const apiHelper = axios.create({
   baseURL
