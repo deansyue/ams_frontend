426eb5e873e6902326f0dfb9bf36c8fffbbeb7fa
deanSyue <c1318131801@gmail.com>
2023-01-04 20:45:34 +0800

    modify: 串接後端url,判斷線上部署與本地測試開發有不同的路徑

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import Swal from 'sweetalert2'
 
-export const baseURL = 'https://stark-temple-81519.herokuapp.com/api'
+export const baseURL = process.env.NODE_ENV === 'production' ? 'https://stark-temple-81519.herokuapp.com/api' : 'http://localhost:3000/api'
 
 export const apiHelper = axios.create({
   baseURL
